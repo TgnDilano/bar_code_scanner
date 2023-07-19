f812bd4188b54c34ea93b284cd1e6a63e200b2cc
guybk21 <139230907+guybk21@users.noreply.github.com>
2023-07-19 18:39:51 +0100

    Update remove_tpe.dart

--- lib/pages/remove_tpe.dart
+++ lib/pages/remove_tpe.dart
@@ -10,12 +10,32 @@ class RemoveTpe extends StatefulWidget {
 class _RemoveTpeState extends State<RemoveTpe> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('remove Tpe'),
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade400,
+        title: const Text(
+          'remove TPE',
+          style: TextStyle(
+              fontWeight: FontWeight.w600,fontSize: 22,color: Colors.white70
+          ),
+        ),
+      ),
+      body: Form(
+        child: Column(children: [
+          TextFormField(
+            decoration:InputDecoration(
+              labelText: 'nom',
+              hintText: 'entrer votre nom',
+            )
+          )
+        ],)
+        
+
+        )
 
       );
   
-    );
+    
     
   }
 }
