94ef13f0f0b8b4aafee120bf589e76bc9c8d3688
guybk21 <139230907+guybk21@users.noreply.github.com>
2023-07-24 11:39:53 +0100

    Update remove_tpe.dart

--- lib/pages/remove_tpe.dart
+++ lib/pages/remove_tpe.dart
@@ -8,30 +8,90 @@ class RemoveTpe extends StatefulWidget {
 }
 
 class _RemoveTpeState extends State<RemoveTpe> {
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: const Text(
-          'remove TPE',
-          style: TextStyle(
-              fontWeight: FontWeight.w600,fontSize: 22,color: Colors.white70
-          ),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text(
+            'remove TPE',
+            style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 22,
+                color: Colors.white70),
+          ),                  
         ),
-      ),
-      body: const Column(
-        children: [
-          Text(
-            'Enter your name',
-          ),
-          TextField()
-        ],
-        )
-      );
-  
+        body: Container(
+            margin: const EdgeInsets.all(15),
+            child: Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration:  const InputDecoration(
+                        labelText: "NAME",
+                        hintText: "please enter your name",
+                        border: OutlineInputBorder(),
+                      ),
+                      //validation du champ
+                      validator: (values) {
+                        if (values == null || values.isEmpty) {
+                          return "error in the text";
+                        }else{
+                          return null;
+                        }
+                      },
+                    ),
+                    const  SizedBox(height: 15),
+                   
+                  TextFormField(
+                      decoration:  const InputDecoration(
+                        labelText: "phone number",
+                        hintText: "please enter your phone number",
+                        border: OutlineInputBorder(),
+                      ),
+                      //validation du champ
+                      validator: (values) {
+                        if (values == null || values.isEmpty) {
+                          return "error in the text";
+                        }else{
+                          return null;
+                      
+                        }
+                        },
+                    ),
+                   const  SizedBox(height: 15),
+                  TextFormField(
+                      decoration:  const InputDecoration(
+                        labelText: "Adress",
+                        hintText: "please enter your Adress",
+                        border: OutlineInputBorder(),
+                      ),
+                      //validation du champ
+                      validator: (values) {
+                        if (values == null || values.isEmpty) {
+                          return "error in the text";
+                        }else{
+                          return null;
+                        }
+                      },
+                    ),
+                    const  SizedBox(height: 15),
+                     ElevatedButton(
+                      onPressed: (){}, 
+                      child:const Text('send')
+                      ),
+                  ],
+                )
+                )
+                ),
+                floatingActionButton: FloatingActionButton.extended(
+                  onPressed: (){}, 
+                  label:const Text('scan'),
+                  icon: const Icon(Icons.camera),
+                  ),
     
-    
+    );
   }
 }
-
